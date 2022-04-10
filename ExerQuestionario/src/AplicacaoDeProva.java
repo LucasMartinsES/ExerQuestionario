7049d9c4a7642fbefab013228d435be910e87bae
Lucas Martins <100964449+LucasMartinsES@users.noreply.github.com>
2022-04-10 16:36:32 -0300

    Update AplicacaoDeProva.java

--- ExerQuestionario/src/AplicacaoDeProva.java
+++ ExerQuestionario/src/AplicacaoDeProva.java
@@ -1,34 +1,3 @@
 public class AplicacaoDeProva extends Question {
-
-    private String RespostasProva;
-    private double NotaProva;
-    private String RelatorioProva;
-
-    //Cadastrar resposta por string
-
-    public RespostasProva() {
-        return null;
-
-
-
-    }
-
-    //Retorna nota da prova
-
-    public NotaProva() {
-        return null;
-
-
-
-    }
-
-    //entrega o relatiório da prova
     
-    public RelatorioProva() {
-        return null;
-
-
-    }
-
-
 }
